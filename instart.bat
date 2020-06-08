5126fa6947a72c761b7ca9612a555b874429cbee
SilenceGold <397135766@qq.com>
2020-06-08 10:31:48 +0800

    删picture_re1\2功能，新增picture_re3功能

---
+++ instart.bat
@@ -0,0 +1,6 @@
+pip install numpy  -i https://pypi.douban.com/simple
+pause
+pip install opencv-python  -i https://pypi.douban.com/simple
+pause
+pip install Pillow   -i https://pypi.douban.com/simple
+pause
